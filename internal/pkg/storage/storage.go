7bd4ff4d591633cdb08611dedfb6e07aff7a1ee9
Nikita Aryuzin <nikitaastro1iq@gmail.com>
2024-10-14 21:58:31 +0300

    Added logging ops, for normal work

--- internal/pkg/storage/storage.go
+++ internal/pkg/storage/storage.go
@@ -2,6 +2,8 @@ package storage
 
 import (
 	"strconv"
+
+	"go.uber.org/zap"
 )
 
 type Variable struct {
@@ -11,21 +13,32 @@ type Variable struct {
 }
 
 type Storage struct {
-	inner map[string]Variable
+	inner  map[string]Variable
+	logger *zap.Logger
 }
 
 func NewStorage() (Storage, error) {
+	logger, cde := zap.NewProduction()
+	if cde != nil {
+		return Storage{}, cde
+	}
+	defer logger.Sync()
+	logger.Info("Created new Storage object")
 	return Storage{
-		inner: make(map[string]Variable),
+		inner:  make(map[string]Variable),
+		logger: logger,
 	}, nil
 }
 
 func (r Storage) Set(key, value string) {
+	defer r.logger.Sync()
 	if digit, err := strconv.Atoi(value); err == nil {
 		r.inner[key] = Variable{integer: digit, type_of_val: "D"}
 	} else {
 		r.inner[key] = Variable{str: value, type_of_val: "S"}
 	}
+	r.logger.Info("Added value on key:", zap.String("key", key), zap.Any("data", value))
+	r.logger.Sync()
 }
 
 func (r Storage) Get_Var(key string) (Variable, bool) {
@@ -38,10 +51,11 @@ func (r Storage) Get_Var(key string) (Variable, bool) {
 
 func (r Storage) Get(key string) *string {
 	res, err := r.Get_Var(key)
-
 	if !err {
 		return nil
 	}
+	r.logger.Info("returned value by key", zap.String("key", key))
+	r.logger.Sync()
 	return &res.str
 }
 
@@ -50,6 +64,8 @@ func (r Storage) GetKind(key string) string {
 	if !cde {
 		return "sth went wrong"
 	}
+	r.logger.Info("returned type of value")
+	r.logger.Sync()
 	return v_strct.type_of_val
 
 }
