36da1e8cec8bd119f1e271e29f9a411442e91767
Nikita Aryuzin <nikitaastro1iq@gmail.com>
2024-10-15 15:17:14 +0300

    Minor changes for GetKind test

--- internal/pkg/storage/storage_test.go
+++ internal/pkg/storage/storage_test.go
@@ -7,7 +7,6 @@ type testCase struct { //testcases for set/get test
 	name  string
 	key   string
 	value string
-	err   bool
 }
 
 type testCaseFT struct { //testcases for GetKind test
@@ -15,12 +14,13 @@ type testCaseFT struct { //testcases for GetKind test
 	key     string
 	value   string
 	type_OV string
-	err     bool
 }
 
 func TestSG(t *testing.T) { // test for set/get procedure
 	cases := []testCase{
-		{"Hello World", "Hello", "World", false},
+		{"Hello World", "Hello", "World"},
+		{"EmpryTest", "Key2", ""},
+		{"Integer test", "key3", "9112001"},
 	}
 	s, cde := NewStorage()
 	if cde != nil {
@@ -40,8 +40,8 @@ func TestSG(t *testing.T) { // test for set/get procedure
 
 func TestTypeOV(t *testing.T) { // test for type of value
 	cases := []testCaseFT{
-		{"case1", "Key1", "Val1", "S", false},
-		{"case2", "Key2", "1", "D", false},
+		{"case1", "Key1", "Val1", "S"},
+		{"case2", "Key2", "1", "D"},
 	}
 	s, err := NewStorage()
 	if err != nil {
@@ -52,6 +52,7 @@ func TestTypeOV(t *testing.T) { // test for type of value
 		t.Run(c.name, func(t *testing.T) {
 			s.Set(c.key, c.value)
 			tval := s.GetKind(c.key)
+
 			if tval != c.type_OV {
 				t.Errorf("types are not equal")
 			}
