25e96c5040d96e25a3410e5aff3546453ad88ee6
Nikita Aryuzin <nikitaastro1iq@gmail.com>
2024-10-15 16:57:38 +0300

    Added benchmarks for Storage struct, struggling with logger

---
+++ storage_benchmark_test.go
@@ -0,0 +1,45 @@
+package benchmarks
+
+import (
+	"Go_lang/internal/pkg/storage"
+	"strconv"
+	"testing"
+)
+
+func BenchmarkGet(b *testing.B) {
+	s, err := storage.NewStorage()
+
+	if err != nil {
+		b.Errorf("new storage: %v", err)
+	}
+	for i := 0; i < b.N; i++ {
+		s.Set(strconv.Itoa(i), strconv.Itoa(i))
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		s.Get(strconv.Itoa(i))
+	}
+}
+
+func BenchmarkSet(b *testing.B) {
+	s, err := storage.NewStorage()
+	b.ResetTimer()
+	if err != nil {
+		b.Errorf("new storage: %v", err)
+	}
+	for i := 0; i < b.N; i++ {
+		s.Set(strconv.Itoa(i), strconv.Itoa(i))
+	}
+}
+
+func BenchmarkSetGet(b *testing.B) {
+	s, err := storage.NewStorage()
+	b.ResetTimer()
+	if err != nil {
+		b.Errorf("new storage %v", err)
+	}
+	for i := 0; i < b.N; i++ {
+		s.Set(strconv.Itoa(i), strconv.Itoa(i))
+		s.Get(strconv.Itoa(i))
+	}
+}
